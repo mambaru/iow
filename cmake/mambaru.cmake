6afa98b03bf427a70bbe682764bac10ba39a8b36
Vladimir Migashko <migashko@gmail.com>
2018-06-29 18:55:16 +0300

    *cmake

--- cmake/mambaru.cmake
+++ cmake/mambaru.cmake
@@ -6,6 +6,21 @@ endif()
 include(mambaopt)
 include(mambalibs)
 
+get_faslib()
+if (NOT IOW_DISABLE_JSON)
+  get_mambaru(wjson WJSON_DIR "")
+else()
+  add_definitions(-DIOW_DISABLE_JSON)
+endif()
+
+if (NOT IOW_DISABLE_LOG)
+  get_mambaru(wlog WLOG_DIR WLOG_LIB)
+else()
+  add_definitions(-DIOW_DISABLE_LOG)
+endif(NOT IOW_DISABLE_LOG)
+
+get_mambaru(wflow WFLOW_DIR WFLOW_LIB)
+
 include_directories(${CMAKE_CURRENT_SOURCE_DIR})
 
 
