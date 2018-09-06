65e33ced612a26c46ee471ffff089d669770ea4d
Vladimir Migashko <migashko@gmail.com>
2018-09-06 13:30:47 +0300

    -data_pool -global_pool

--- iow/io/rw/ad_initialize.hpp
+++ iow/io/rw/ad_initialize.hpp
@@ -3,7 +3,6 @@
 #include <iow/io/rw/tags.hpp>
 #include <iow/io/reader/data/tags.hpp>
 #include <iow/io/writer/data/tags.hpp>
-#include <iow/io/aux/global_pool.hpp>
 namespace iow{ namespace io{ namespace rw{
 
 struct ad_initialize
@@ -11,10 +10,10 @@ struct ad_initialize
   template<typename T, typename O>
   void operator()(T& t, O opt) const
   {
-    opt.reader.create = global_pool::get_create();
-    opt.reader.free = global_pool::get_free();
-    opt.writer.create = global_pool::get_create();
-    opt.writer.free = global_pool::get_free();
+    opt.reader.create = nullptr;
+    opt.reader.free = nullptr;
+    opt.writer.create = nullptr;
+    opt.writer.free = nullptr;
     t.get_aspect().template get< ::iow::io::reader::data::_initialize_ >()(t, opt.reader);
     t.get_aspect().template get< ::iow::io::writer::data::_initialize_ >()(t, opt.writer);
   }
