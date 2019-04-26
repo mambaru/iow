16a5b02a78cd73ab007a2a8261f0b1968f9db070
Vladimir Migashko <migashko@gmail.com>
2019-04-26 13:54:38 +0300

    wrn fix 4.9

--- iow/io/acceptor/ad_async_accept.hpp
+++ iow/io/acceptor/ad_async_accept.hpp
@@ -10,10 +10,10 @@ struct ad_async_accept
   template<typename T, typename P, typename H>
   void operator()(T& t, P p, H handler) const
   {
-#if defined(__GNUC__) && ( __GNUC__ < 5 )
-    t.descriptor().async_accept( p->descriptor(), std::move(handler), static_cast<void*>(0) );
-#else
+#if defined(__GNUC__) && (__GNUC___ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9))
     t.descriptor().async_accept( p->descriptor(), std::move(handler), nullptr );
+#else
+    t.descriptor().async_accept( p->descriptor(), std::move(handler), static_cast<void*>(0) );
 #endif
   }
 };
