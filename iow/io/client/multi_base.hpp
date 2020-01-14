00502b1ad834486706b16b5343b5764d7b4761d2
Vladimir Migashko <migashko@gmail.com>
2020-01-14 13:30:38 +0300

    Generated export autocommit from mamba.ru

--- iow/io/client/multi_base.hpp
+++ iow/io/client/multi_base.hpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <memory>
 #include <vector>
+#include <iostream>
 
 namespace iow{ namespace io{ namespace client{
 
@@ -16,19 +17,20 @@ public:
   typedef std::vector<client_ptr> client_list;
   typedef typename client_type::io_service_type io_service_type;
   typedef std::recursive_mutex mutex_type;
-  
+
   explicit multi_base(io_service_type& io)
     : _current(0)
     , _io_service(io)
   {}
-  
+
   template<typename Opt>
   void start(Opt opt, int count)
   {
     std::lock_guard<mutex_type> lk(_mutex);
+
     if ( !_clients.empty() )
       return;
-    
+
     _clients.reserve( size_t(count) );
     for (int i = 0; i < count; ++i)
     {
@@ -46,7 +48,7 @@ public:
       _clients.back()->stop();
       _clients.pop_back();
     }
-    
+
     while ( int( _clients.size() ) < count )
     {
       _clients.push_back( std::make_shared<client_type>(_io_service) );
@@ -61,10 +63,10 @@ public:
       std::lock_guard<mutex_type> lk(_mutex);
       _clients.swap(clients);
     }
-
     for ( auto& conn : clients )
     {
       conn->stop();
+      conn.reset();
     }
   }
 
@@ -79,7 +81,7 @@ public:
     }
     return cli->send( std::move(d) );
   }
-  
+
 private:
   size_t _current;
   io_service_type& _io_service;
